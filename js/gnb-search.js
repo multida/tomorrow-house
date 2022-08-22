08efca8707c1bf1fdd97c189eabde666d7b6f45d
multida <wow_da65@naver.com>
2022-08-22 23:13:34 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,21 +1,35 @@
 const gnbSearch = document.querySelector(".gnb-search");
 const gnbSearchInput = gnbSearch.querySelector("input");
-const gnbsearchHistory = document.querySelector(".search-history");
+const gnbSearchHistory = gnbSearch.querySelector(".search-history");
 
+const deleteAllBtn = gnbSearchHistory.querySelector(".search-history-header button");
+const gnbSearchHistoryList =gnbSearchHistory.querySelector('.search-history-list');
 
 function closeGnbSearchHistoryOnClickingOutside(e){
-  console.log("dd")
   if(!gnbSearch.contains(e.target)) {
-    gnbsearchHistory.classList.remove('is-active');
+    gnbSearchHistory.classList.remove('is-active');
     window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside);
   }
 }
 
 function openGnbSearchHistory(){
-  if(!gnbsearchHistory.classList.contains('is-active')) {
+  //check!! => gnbSearchHistoryList에 li가 몇개가 있는지  
+  //li가 0 => 실행 X
+  //li가 1~ => 실행 O
+  if(gnbSearchHistoryList.children.length === 0) {
+    return; //다음의 코드 다 멈춰!!
+  }
+  if(!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside);
   }
-  gnbsearchHistory.classList.add('is-active');
+  gnbSearchHistory.classList.add('is-active');
+}
+
+function deleteAllSearchHistoryItems(){
+  //gnbSearchHistoryList 안에 들어있는 모든 li를 삭제해!
+  gnbSearchHistoryList.innerHTML = '';//빈 문자열로 변경
+  gnbSearchHistory.classList.remove('is-active');
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+deleteAllBtn.addEventListener('click', deleteAllSearchHistoryItems);
