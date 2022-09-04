5b80febfff3ca1b3d3341062c189e5e81753caf8
multida <wow_da65@naver.com>
2022-09-04 21:35:43 +0900

    🚧 Detect positions of tab-panels

--- js/product-tab.js
+++ js/product-tab.js
@@ -35,3 +35,46 @@ productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab);
   button.addEventListener('click', scollToTabPanel);
 })
+
+
+//각 tabPaneldml y축 위치(문서의 시작점에서부터 얼마나 아래에 있는지) -> window.scrollY
+// 요소의 y축 위치 = window.scrollY + element.getBoundingClientRect().top
+
+const productTabPanelIdList = [
+  'product-spec',
+  'product-review',
+  'product-inquiry',
+  'product-shipment',
+  'product-recommendation'
+]
+
+const productTabPanelList = productTabPanelIdList.map((panelId) => {
+  //return 필수 그 return값들로 새로운 배열을 형성한다
+  const tabPanel = document.querySelector(`#${panelId}`);
+  return tabPanel;
+});
+
+const productTabPanelPositionMap = {};
+
+function detectTabPanelPosition() {
+  //각각의 tabPanel의 y축 위치를 찾는다
+  //productTabPanelPositionMap에 그 값을 업데이트 
+  /*
+    예를 들어 
+    {
+      'product-spec': 1000,
+      'product-review' : 5000,
+      ...
+    }
+  */
+
+    productTabPanelList.forEach((panel) => {
+      //id : y축 위치 
+      const id = panel.getAttribute('id');
+      const position = window.scrollY + panel.getBoundingClientRect().top;
+      productTabPanelPositionMap[id] = position;
+    })
+}
+
+window.addEventListener('load', detectTabPanelPosition);//모든 페이지가 다 로드 됐을때 함수 실행
+window.addEventListener('resize', detectTabPanelPosition);//화면 사이즈가 바뀌면 업데이트
